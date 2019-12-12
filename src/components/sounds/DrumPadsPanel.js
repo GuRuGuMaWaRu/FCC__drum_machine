dacb6466bf7574c53469b13ecbfb49143e251bf8
GuRuGu <peter.k.nets@gmail.com>
2019-12-12 12:37:56 +0200

    Add Roboto as font & update TODOs in README

--- src/components/sounds/DrumPadsPanel.js
+++ src/components/sounds/DrumPadsPanel.js
@@ -6,38 +6,39 @@ const DrumPadsPanel = () => {
   const drumPads = [
     {
       name: "Q",
-      src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3"
+      src: "https://s3.amazonaws.com/freecodecamp/drums/Chord_1.mp3"
     },
     {
       name: "W",
-      src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3"
+      src: "https://s3.amazonaws.com/freecodecamp/drums/Chord_2.mp3"
     },
     {
       name: "E",
-      src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3"
+      src: "https://s3.amazonaws.com/freecodecamp/drums/Chord_3.mp3"
     },
     {
       name: "A",
-      src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3"
+      src: "https://s3.amazonaws.com/freecodecamp/drums/Give_us_a_light.mp3"
     },
     {
       name: "S",
-      src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3"
+      src: "https://s3.amazonaws.com/freecodecamp/drums/Dry_Ohh.mp3"
     },
     {
       name: "D",
-      src: "https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3"
+      src: "https://s3.amazonaws.com/freecodecamp/drums/Bld_H1.mp3"
     },
     {
       name: "Z",
-      src: "https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3"
+      src: "https://s3.amazonaws.com/freecodecamp/drums/punchy_kick_1.mp3"
     },
     {
       name: "X",
-      src: "https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3"
+      src: "https://s3.amazonaws.com/freecodecamp/drums/side_stick_1.mp3"
     },
-    { name: "C", 
-      src: "https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3" 
+    {
+      name: "C",
+      src: "https://s3.amazonaws.com/freecodecamp/drums/Brk_Snr.mp3"
     }
   ];
 
